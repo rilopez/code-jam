4e9793416d0bc66037c6ec9eb5f18c12349b5d7b
ilopez <ilopez@switchfly.com>
2014-04-12 09:42:00 -0700

    trying to solve case 5 ********** ********** ********** ****....** ***.....** ***.c...** ***....*** ********** ********** **********

--- src/main/java/com/nearsoft/codejam/qualificationRound2014/MinesweeperMaster/MinesweeperMaster.java
+++ src/main/java/com/nearsoft/codejam/qualificationRound2014/MinesweeperMaster/MinesweeperMaster.java
@@ -11,6 +11,9 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class MinesweeperMaster {
+
+    public static final String IMPOSSIBLE = "Impossible";
+
     public String execute(Reader reader) {
         int totalCases = 0;
 
@@ -29,7 +32,7 @@ public class MinesweeperMaster {
                     int columns = scanner.nextInt();
                     int numberOfMines = scanner.nextInt();
 
-                    sb.append("Case #").append(testCasesCounter).append(": ");
+                    sb.append("Case #").append(testCasesCounter).append(":\n");
                     sb.append(findConfigurationForOneClick(rows, columns, numberOfMines));
                     sb.append("\n");
                 }
@@ -42,9 +45,10 @@ public class MinesweeperMaster {
 
     private String findConfigurationForOneClick(int rows, int columns, int numberOfMines) {
         if (numberOfMines >= rows * columns) {
-            return "Impossible";
+            return IMPOSSIBLE;
         }
         char[][] board = new char[rows][columns];
+
         //set the mines
         int mineR = rows - 1;
         int mineC = columns - 1;
@@ -52,7 +56,7 @@ public class MinesweeperMaster {
             board[mineR][mineC] = '*';
             mineC--;
             if (mineC < 0) {
-                mineC = rows - 1;
+                mineC = columns - 1;
                 mineR--;
             }
         }
@@ -63,7 +67,7 @@ public class MinesweeperMaster {
             for (int c = 0; c < columns; c++) {
                 char currentCell = board[r][c];
                 if (currentCell != '*') {
-                    char neighborMines = countNeighborMines(r, c, board, rows, columns);
+                    char neighborMines = Integer.toString(countNeighborChars(r, c, board, '*')).charAt(0);
                     board[r][c] = neighborMines;
                     if (!positionIndex.containsKey(neighborMines)) {
                         positionIndex.put(neighborMines, new ArrayList<List<Integer>>());
@@ -75,70 +79,112 @@ public class MinesweeperMaster {
 
         int rFastWinnerClick = -1;
         int cFastWinnerClick = -1;
-        for (int i = 0; i <= 8 && rFastWinnerClick < 0 && cFastWinnerClick < 0; i++) {
-            char iAsChar = Integer.toString(i).charAt(0);
-            if (positionIndex.containsKey(iAsChar)) {
-                List<List<Integer>> listOfPositionForChar = positionIndex.get(iAsChar);
-                for (List<Integer> position : listOfPositionForChar) {
-                    //try to solve with one click
-                    if (remainHiddenCellsAfterClickOn(position.get(0),position.get(1),board) == 0 ){
-                        rFastWinnerClick = position.get(0);
-                        cFastWinnerClick = position.get(1);
+        if (positionIndex.containsKey('0')) {
+            List<List<Integer>> positionsWithZero = positionIndex.get('0');
+            boolean allZerosAreConnected = true;
+            if (positionsWithZero.size() > 1) {
+                for (List<Integer> position : positionsWithZero) {
+                    if (countNeighborChars(position.get(0), position.get(1), board, '0') == 0) {
+                        allZerosAreConnected = false;
                         break;
                     }
+                }
+            }
+            if (allZerosAreConnected && allOtherNumbersHaveAZeroNeighbor(board)) {
+                List<Integer> firstPosition = positionsWithZero.get(0);
+                rFastWinnerClick = firstPosition.get(0);
+                cFastWinnerClick = firstPosition.get(1);
+            }
+        } else if (numberOfMines + 1 == rows * columns && positionIndex.values().size() > 0) {
+            List<Integer> anyPosition = positionIndex.values().iterator().next().get(0);
+            rFastWinnerClick = anyPosition.get(0);
+            cFastWinnerClick = anyPosition.get(1);
+        }
 
+        if (rFastWinnerClick == -1 || cFastWinnerClick == -1) {
+            return IMPOSSIBLE  + '\n' + printSolvedBoard(rFastWinnerClick, cFastWinnerClick, board);
+        } else {
+            return printSolvedBoard(rFastWinnerClick, cFastWinnerClick, board);
+        }
+    }
+
+    private boolean allOtherNumbersHaveAZeroNeighbor(char[][] board) {
+        boolean allHaveAZeroNeighbor = true;
+        for (int r = 0; r < board.length && allHaveAZeroNeighbor; r++) {
+            for (int c = 0; c < board[r].length; c++) {
+                char cell = board[r][c];
+                if (cell != '0' && cell != '*' && countNeighborChars(r, c, board, '0') == 0) {
+                    allHaveAZeroNeighbor = false;
+                    break;
                 }
             }
         }
-
-        return null;
+        return allHaveAZeroNeighbor;
     }
 
-    private int remainHiddenCellsAfterClickOn(int r, int c, char[][] board) {
+    private String printSolvedBoard(int rFastWinnerClick, int cFastWinnerClick, char[][] board) {
+        StringBuilder sb = new StringBuilder();
 
-        return 0;  //To change body of created methods use File | Settings | File Templates.
+        for (int r = 0; r < board.length; r++) {
+            for (int c = 0; c < board[r].length; c++) {
+                if (board[r][c] == '*') {
+                    sb.append('*');
+                } else if (r == rFastWinnerClick && c == cFastWinnerClick) {
+                    sb.append('c');
+                } else {
+                    //sb.append('.');
+                    sb.append(board[r][c]);
+                }
+            }
+            if (r + 1 < board.length) {
+                sb.append('\n');
+            }
+        }
+        return sb.toString();
     }
 
-    private char countNeighborMines(int r, int c, char[][] board, int rows, int columns) {
+    private int countNeighborChars(int r, int c, char[][] board, char character) {
+        int rows = board.length;
+        int columns = board[0].length;
         int mines = 0;
         //6 o'clock
-        if (r + 1 < rows && board[r + 1][c] == '*') {
+        if (r + 1 < rows && board[r + 1][c] == character) {
             mines++;
         }
         //4:30
-        if (r + 1 < rows && c + 1 < columns && board[r + 1][c + 1] == '*') {
+        if (r + 1 < rows && c + 1 < columns && board[r + 1][c + 1] == character) {
             mines++;
         }
 
         // 3 o'clock
-        if (c + 1 < columns && board[r][c + 1] == '*') {
+        if (c + 1 < columns && board[r][c + 1] == character) {
             mines++;
         }
 
         //1:30
-        if (r - 1 >= 0 && c + 1 < columns && board[r - 1][c + 1] == '*') {
+        if (r - 1 >= 0 && c + 1 < columns && board[r - 1][c + 1] == character) {
             mines++;
         }
 
         //12 o'clock
-        if (r - 1 >= 0 && board[r - 1][c] == '*') {
+        if (r - 1 >= 0 && board[r - 1][c] == character) {
             mines++;
         }
 
         //10:30
-        if (r - 1 >= 0 && c - 1 >= 0 && board[r - 1][c - 1] == '*') {
+        if (r - 1 >= 0 && c - 1 >= 0 && board[r - 1][c - 1] == character) {
             mines++;
         }
 
         //9 o'clock
-        if (c - 1 >= 0 && board[r][c - 1] == '*') {
+        if (c - 1 >= 0 && board[r][c - 1] == character) {
             mines++;
         }
 
         //7:30
-        if (r + 1 < rows && c - 1 >= 0 && board[r + 1][c - 1] == '*') {
+        if (r + 1 < rows && c - 1 >= 0 && board[r + 1][c - 1] == character) {
             mines++;
         }
-        return Integer.toString(mines).charAt(0);
+        return mines;
     }
 }
